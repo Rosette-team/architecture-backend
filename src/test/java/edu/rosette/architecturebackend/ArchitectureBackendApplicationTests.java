09a2bf04cd8a826b4c94a7eb8edffc609b513d5d
BFDestroyeer <mzoreev@gmail.com>
2022-11-01 11:25:18 +0300

    Project initialization

---
+++ src/test/java/edu/rosette/architecturebackend/ArchitectureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rosette.architecturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
