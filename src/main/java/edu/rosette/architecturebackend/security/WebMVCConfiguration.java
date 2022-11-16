7a866e06b9c613f88329fcad80bef2236a5f81a4
BFDestroyeer <mzoreev@gmail.com>
2022-11-16 17:37:51 +0300

    Disabled CORS

---
+++ src/main/java/edu/rosette/architecturebackend/security/WebMVCConfiguration.java
@@ -0,0 +1,19 @@
+package edu.rosette.architecturebackend.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMVCConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry
+                .addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
